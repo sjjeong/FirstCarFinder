033e77d25e3264428d134e70eaa380c5e4079dd2
sjjeong <sjjeong1225@gmail.com>
2019-08-18 21:56:42 +0900

    CarFinderFragment 기본 구성 추가

---
+++ app/src/main/java/com/googry/firstcarfinder/ui/carfinder/CarFinderFragment.kt
@@ -0,0 +1,12 @@
+package com.googry.firstcarfinder.ui.carfinder
+
+import com.dino.library.ui.BaseFragment
+import com.googry.firstcarfinder.R
+import com.googry.firstcarfinder.databinding.FragmentCarFinderBinding
+import org.koin.androidx.viewmodel.ext.android.viewModel
+
+class CarFinderFragment : BaseFragment<FragmentCarFinderBinding, CarFinderViewModel>(
+    R.layout.fragment_car_finder
+) {
+    override val viewModel by viewModel<CarFinderViewModel>()
+}
---
+++ app/src/main/java/com/googry/firstcarfinder/ui/carfinder/CarFinderViewModel.kt
@@ -0,0 +1,7 @@
+package com.googry.firstcarfinder.ui.carfinder
+
+import com.dino.library.ui.BaseViewModel
+
+class CarFinderViewModel : BaseViewModel(){
+
+}
