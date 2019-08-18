11c70f414ee2d27a04234015b65e1c602e9121ad
sjjeong <sjjeong1225@gmail.com>
2019-08-18 22:02:14 +0900

    CarFinderViewModel를 di module에 추가

--- app/src/main/java/com/googry/firstcarfinder/di/ViewModelModule.kt
+++ app/src/main/java/com/googry/firstcarfinder/di/ViewModelModule.kt
@@ -1,7 +1,9 @@
 package com.googry.firstcarfinder.di
 
+import com.googry.firstcarfinder.ui.carfinder.CarFinderViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val viewModelModule = module {
-
+    viewModel { CarFinderViewModel() }
 }
