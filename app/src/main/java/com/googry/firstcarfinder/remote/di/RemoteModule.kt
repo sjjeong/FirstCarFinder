da48ad99ab33e7c2ddafe67474df95d98b341dce
sjjeong <sjjeong1225@gmail.com>
2019-08-26 01:35:23 +0900

    DaumAutoApi di 추가

--- app/src/main/java/com/googry/firstcarfinder/remote/di/RemoteModule.kt
+++ app/src/main/java/com/googry/firstcarfinder/remote/di/RemoteModule.kt
@@ -1,9 +1,12 @@
 package com.googry.firstcarfinder.remote.di
 
 import com.googry.firstcarfinder.data.remote.DaumAutoRemoteDataSource
+import com.googry.firstcarfinder.remote.api.DaumAutoApi
 import com.googry.firstcarfinder.remote.impl.DaumAutoRemoteDataSourceImpl
 import org.koin.dsl.module
+import retrofit2.Retrofit
 
 val remoteModule = module {
+    single<DaumAutoApi> { get<Retrofit>().create(DaumAutoApi::class.java) }
     single<DaumAutoRemoteDataSource> { DaumAutoRemoteDataSourceImpl(get()) }
 }
